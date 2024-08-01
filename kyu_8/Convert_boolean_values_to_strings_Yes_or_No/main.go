105b36f7423c3e030080f9e31647c7d10b67dfc0
KotopheiOP <kotopheiop@mail.ru>
2024-08-01 10:21:28 +0500

    7 - 8 kyu

---
+++ kyu_8/Convert_boolean_values_to_strings_Yes_or_No/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func BoolToWord(word bool) string {
+	if word {
+		return "Yes"
+	}
+
+	return "No"
+}
+
+func main() {
+	fmt.Println(BoolToWord(true))  // Yes
+	fmt.Println(BoolToWord(false)) // No
+}
