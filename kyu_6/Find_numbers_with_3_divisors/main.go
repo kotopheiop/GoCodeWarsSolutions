ab0aad050cdc620df5ab15e47e0141c9e8700a8c
KotopheiOP <kotopheiop@mail.ru>
2024-08-06 09:36:34 +0500

    6,8 kyu

---
+++ kyu_6/Find_numbers_with_3_divisors/main.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"sort"
+)
+
+// Решение не засчитано
+// Ссылка на кату: https://www.codewars.com/kata/65eb2c4c274bd91c27b38d32
+// Разобраться с большими числами
+
+func sieveOfEratosthenes(limit int) []int {
+	isPrime := make([]bool, limit+1)
+	for i := range isPrime {
+		isPrime[i] = true
+	}
+	isPrime[0], isPrime[1] = false, false
+
+	for p := 2; p*p <= limit; p++ {
+		if isPrime[p] {
+			for i := p * p; i <= limit; i += p {
+				isPrime[i] = false
+			}
+		}
+	}
+
+	var primes []int
+	for p := 2; p <= limit; p++ {
+		if isPrime[p] {
+			primes = append(primes, p)
+		}
+	}
+
+	return primes
+}
+
+func Solution(n, m int) []int {
+	limit := int(math.Pow(float64(m), 1.0/4.0))
+	primes := sieveOfEratosthenes(limit)
+
+	var result []int
+	for _, p := range primes {
+		pFourth := p * p * p * p
+		if pFourth >= n && pFourth <= m {
+			result = append(result, pFourth)
+		}
+	}
+
+	if len(result) == 0 {
+		return []int{}
+	}
+
+	sort.Slice(result, func(i, j int) bool { return result[i] < result[j] })
+
+	return result
+}
+
+func main() {
+	fmt.Println(Solution(2, 100))        // [16, 81]
+	fmt.Println(Solution(10000, 100000)) // [14641, 28561, 83521]
+	fmt.Println(Solution(624, 625))      // [625]
+	fmt.Println(Solution(625, 626))      // [625]
+	fmt.Println(Solution(734, 735))      // []
+}
