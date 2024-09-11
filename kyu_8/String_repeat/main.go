4b87e913ac3439eab76da02c733ea6d8f849151a
Semyon Borisov <kotopheiop@mail.ru>
2024-09-11 13:07:59 +0500

    String repeat

---
+++ kyu_8/String_repeat/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func RepeatStr(repetitions int, value string) string {
+	return strings.Repeat(value, repetitions)
+}
+
+func main() {
+	fmt.Println(RepeatStr(4, "a"))      // aaaa
+	fmt.Println(RepeatStr(3, "hello ")) // hello hello hello
+}
